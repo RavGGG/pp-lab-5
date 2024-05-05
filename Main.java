a23ea2e6536f24b2bfbe722b4be9ec697ede6415
Rafal <rafalgad@wp.pl>
2024-05-05 15:52:08 +0200

    zad4

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import company.beans.Person;
+import company.exceptions.InvalidAgeException;
 import company.implementations.EmailMessenger;
 import company.utils.MathUtils;
 
@@ -32,6 +33,29 @@ public class Main {
         }
 
 
+        Person[] people = new Person[5];
+        int b = 10;
+
+        try {
+            people[0] = new Person("John Doe", 30);
+            people[1] = new Person("John AAA", 50);
+            people[2] = new Person("John BBB", 45);
+            people[3] = new Person("John CCC", 18);
+            people[4] = new Person("John DDD", 120);
+
+            for (Person person : people) {
+                int newAge = person.getAge() + b;
+                if (newAge > 125) {
+                    System.out.println("ERROR:age after calculation exceeds the range " + person.getName() +" " + newAge);
+                } else {
+                    result = MathUtils.add(person.getAge(), b);
+                messenger.sendMessage(person.getName() + " age before: " + person.getAge() + ", age after calculation: " + result);
+                }
+                
+            }
+        } catch (InvalidAgeException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
 
 
